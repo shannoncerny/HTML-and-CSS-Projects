ddb15ba330eafc43e87d2fdd3c1236b518fcd18c
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2022-11-29 10:55:24 -0600

    Basic HTML and JavaScript 1 Submission Assignment

--- Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -1,5 +1,5 @@
 <script>
-	function My_First_Function() {
+	function My_First_Function(){
 		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
 		document.getElementById("Irish").innerHTML =
